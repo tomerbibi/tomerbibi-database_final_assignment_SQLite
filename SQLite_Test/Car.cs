51a30f3ff13c6a72108dbc17f6331b42430654c6
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2021-01-12 15:37:01 +0200

    Add files via upload

---
+++ SQLite_Test/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_Test
+{
+    class Car
+    {
+        public Car(int id, string manufacturer, string model, int year)
+        {
+            Id = id;
+            Manufacturer = manufacturer;
+            Model = model;
+            this.year = year;
+        }
+
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int year { get; set; }
+    }
+}
